72efa239a7ff31d3362658ca15eea9ae26cad90c
unknown <ale.cantarero@unitec.edu>
2021-11-26 16:06:50 -0600

    Avance 9

--- src/lab7p2_emiliocantarero_12111141/BaseDeDatos.java
+++ src/lab7p2_emiliocantarero_12111141/BaseDeDatos.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab7p2_emiliocantarero_12111141;
 
-/**
- *
- * @author Emilio Cantarero
- */
+import java.util.ArrayList;
+
+
 public class BaseDeDatos {
+    ArrayList<Accesorio> inventario = new ArrayList();
+
+    public BaseDeDatos() {
+    }
+
+    public ArrayList<Accesorio> getInventario() {
+        return inventario;
+    }
+
+    public void setInventario(ArrayList<Accesorio> inventario) {
+        this.inventario = inventario;
+    }
     
+    public void agregar(Accesorio a){
+        inventario.add(a);
+    }
+    
+    public void eliminar(int id){
+        for (Accesorio temp : inventario) {
+            if (temp.getId()==id){
+                inventario.remove(temp);
+                break;
+            }
+        }
+    }
+    
+    public void modificarCantidad(Accesorio a, int cantidad){
+        for (Accesorio temp : inventario) {
+            if (temp.equals(a)){
+                temp.setCantidad(temp.getCantidad()-cantidad);
+                break;
+            }
+        }
+    }
 }
